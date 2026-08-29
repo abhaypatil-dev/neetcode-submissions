51201f083016969c590867d469e9cb4c4954aec9
Abhay Patil <abhaypatil2204.ai@gmail.com>
2026-08-29 18:50:37 +0530

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.py
@@ -0,0 +1,31 @@
+class Solution:
+    # osheen, naina
+    # 6osheen5naina
+    # Encode: osheen|naina
+
+    # 6osheen, 10naina
+    # 7(6osheen)7(10naina)
+    # Encode: osheen abhay naina 
+
+    def encode(self, strs: List[str]) -> str:
+        ans = ""
+        for s in strs:
+            ans += f'{len(s)}|{s}'
+        return ans
+
+    def decode(self, s: str) -> List[str]:
+        ans = []
+        i = 0
+        # print(s)
+        while i < len(s):
+            num = ''
+            while s[i] != '|':
+                num += s[i]
+                i += 1
+            n = int(num)
+
+            ans.append(s[i + 1:i + 1 + n])
+            # print(i, ans)
+            i += n + 1
+        return ans
+        
